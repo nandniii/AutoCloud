8cbd997f10a383fd2386bbcee987489ae24e9c4b
Nandani <nandnip15@gmail.com>
2025-11-01 01:51:06 +0530

    Fix alias config, add utils, update Vite setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login  from './login'
+import Login  from './components/login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div ><Login/></div>
+      <div><Login /></div>
       {/* <Routes>
         <Route path='login' element={<login/>}></Route>
       </Routes> */}
